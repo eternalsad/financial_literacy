5a75fe26cad9030a4e8f47796c48cfdb7ccc00d6
erictronic <sadgalin.e@gmail.com>
2022-02-19 23:43:08 +0600

    fix: check if table exists

--- database/DBIsEmpty.go
+++ database/DBIsEmpty.go
@@ -6,18 +6,18 @@ import (
 
 // IsEmpty checks if a database is empty
 func IsEmpty(db *sql.DB) bool {
-	rows, err := db.Query("SELECT * FROM transactions;")
+	_, err := db.Query("SELECT * FROM transactions;")
 	if err != nil {
 		return false
 	}
-	for rows.Next() {
-		var id int
-		if err := rows.Scan(&id); err != nil {
-			return false
-		}
-		if id == 0 {
-			return false
-		}
-	}
+	// for rows.Next() {
+	// 	var id int
+	// 	if err := rows.Scan(&id); err != nil {
+	// 		return false
+	// 	}
+	// 	if id == 0 {
+	// 		return false
+	// 	}
+	// }
 	return true
 }
